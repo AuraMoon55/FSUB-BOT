9daa5a204b54e96efc06f04288727f64751fef0e
Anshul Garg <garganshul553@gmail.com>
2022-05-31 21:12:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = getenv("API_ID")
+API_HASH = getenv("API_HASH")
+TOKEN = getenv("BOT_TOKEN")
+DB_CHANNEL = getenv("DB_CHANNEL")
+FSUB = getenv("FSUB_CHANNEL")
